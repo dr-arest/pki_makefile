c87d1d1abce739c6e1561db4d68ca0eb16ff45d0
root <root@home-nas>
2019-01-15 19:13:21 +0200

    True first commit(with data)

---
+++ passwords.mk
@@ -0,0 +1,7 @@
+PASSWORD_SERVER	= 'shoamiphio0o'
+PASSWORD_ROOT_CA	= 'PerekatipoleRoot'
+PASSWORD_SUB_CA	= 'PerekatipoleSub'
+PASSWORD_CLIENT	= 'Mistake+'
+USER_NAME	= 'arest'
+ROOT_CA_DEFAULT_DAYS  = '3650'
+SUB_CA_DEFAULT_DAYS  = '3650'
